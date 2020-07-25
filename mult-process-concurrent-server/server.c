ff0cf390d3c1483fdcfccc98a155cde7f2cc8759
JakeLin <jakelin0fly@gmail.com>
2020-07-25 13:43:17 +0800

    多进程并发服务器demo-信号机制解决僵尸进程bug

--- mult-process-concurrent-server/server.c
+++ mult-process-concurrent-server/server.c
@@ -1,3 +1,11 @@
+/**
+********************多并发服务器demo********************
+父进程：监听等待接收客户端连接
+子进程：处理客户端请求
+
+信号处理：解决子进程结束后成为僵尸进程问题
+*******************************************************
+**/
 #include <unistd.h>
 #include <stdio.h>
 #include <arpa/inet.h>
@@ -6,9 +14,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
 
+#define SERVER_IP	"127.0.0.1"
 #define SERVER_PORT	8888
 #define BUF_SIZE 1024
+
+/** 信号处理 回收子进程 **/
+void sig_wait_child(int signo) {
+	//0 回收同用户组的任意子进程
+	//WNOHANG 非阻塞
+	while(waitpid(0, NULL, WNOHANG) > 0);
+
+	return ;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -17,12 +37,13 @@ int main(int argc, char const *argv[])
 	int ret, n;
 	socklen_t client_addr_len;
 	pid_t pid;
-	char buf[BUF_SIZE];
+	char buf[BUF_SIZE], clinet_IP[BUF_SIZE];
+	// struct sigaction new_action, old_action;
 
 	//第一步：socket()
 	l_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if(-1 == l_socket) {
-		perror("socket error: ");
+		perror("socket error");
 		exit(1);
 	}
 
@@ -30,21 +51,27 @@ int main(int argc, char const *argv[])
 	bzero(&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(SERVER_PORT);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	//or
-	//inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr);
+	inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr.s_addr);
 	ret = bind(l_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if(-1 == ret) {
-		perror("bind error: ");
+		perror("bind error");
 		exit(1);
 	}
 
 	//第三步：listen()	系统最大支持128
 	ret = listen(l_socket, 128);
 	if(-1 == ret) {
-		perror("listen error: ");
+		perror("listen error");
 		exit(1);
 	}
+
+	// //注册信号处理
+	// new_action.sa_handler = sig_wait_child;
+	// sigemptyset(&new_action.sa_mask);
+	// sigaddset(&new_action.sa_mask, SIGCHLD);
+	// new_action.sa_flags = 0;
 
 	while(true) {
 		//第四步：accept()
@@ -53,15 +80,19 @@ int main(int argc, char const *argv[])
 
 		pid = fork();
 		if(pid < 0) {
-			perror("fork error: ");
+			perror("fork error");
 			exit(1);
 		}
 		else if(pid == 0) { //子进程
 			break;
 		} else {	//父进程
-		//	printf("client %s:%d\n", 
-		//		inet_ntop(AF_INET, &client_addr.sin_addr.s_addr);
-			close(c_socket);
+			close(c_socket);	//关闭已连接的套接字
+//			sigaction(SIGCHLD, &new_action, &old_action);
+			signal(SIGCHLD, sig_wait_child);
+			printf("client %s:%d\n", 
+				inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, clinet_IP, sizeof(clinet_IP)),
+					ntohs(client_addr.sin_port)
+					);
 		}
 	}
 //子进程 处理连接
@@ -74,8 +105,10 @@ read_again:
 				if(errno == EINTR)
 					goto read_again;
 				else {
-					perror("listen error: ");
-					exit(1);
+					perror("read error");
+					close(c_socket);
+					exit(1);	//不关闭文件描述符 子进程使用exit无法终止
+					// _exit(1);	//不检查文件的打开情况，强行退出进程
 				}
 			}
 			else if(0 == n) {
