b1b2b483a2fa31cedb9b4ac263b52832e5ed6fed
JakeLin <jakelin0fly@gmail.com>
2021-02-03 15:35:01 +0800

    条件满足信号丢失

---
+++ sync/cond_signal_timeout.cpp
@@ -0,0 +1,55 @@
+/**
+ * 【场景】条件变量的信号产生后，子线程才阻塞于等待条件满足
+ * 【结论】条件满足信号丢失，子线程一直阻塞
+**/
+
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+
+using namespace std;
+
+struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond; 
+} mutex_cond = {
+    PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER
+};
+
+
+void *thread_son (void *arg) {
+    sleep(4); // 等主线程 signal 结束
+    pthread_mutex_lock(&mutex_cond.mutex);
+    cout << "子线程：获取到互斥锁" << endl;
+    
+    cout << "子线程：尝试获取条件变量..." << endl;
+    pthread_cond_wait(&mutex_cond.cond, &mutex_cond.mutex);
+    cout << "子线程：获取到条件变量！" << endl;
+
+    pthread_mutex_unlock(&mutex_cond.mutex);
+    cout << "子线程：释放掉互斥锁" << endl;
+
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t tid;
+
+    pthread_create(&tid, NULL, thread_son, NULL);
+    cout << "主线程：子线程创建完毕" << endl;
+    sleep(2); // 保证子线程获得互斥锁并阻塞在条件变量上
+
+    cout << "主线程：发出 signal... " << endl;
+    pthread_cond_signal(&mutex_cond.cond);
+
+    // pthread_mutex_lock(&mutex_cond.mutex);
+    // cout << "主线程：获取到互斥锁" << endl;
+
+    // pthread_mutex_unlock(&mutex_cond.mutex);
+    // cout << "主线程：释放掉互斥锁" << endl;
+
+    pthread_join(tid, NULL); // 等待回收子线程
+
+    return 0;
+}
